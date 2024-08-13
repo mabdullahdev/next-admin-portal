3c34e1393c2a8c37cc5f390b7047476d688f47f4
samiUllah732 <samiikonic@gmail.com>
2024-08-13 22:04:00 +0500

    login page form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import LoginForm from '@/components/forms/LoginForm';
+import Image from 'next/image';
 
 const LoginPage: React.FC = () => {
   return (
-    <>
-      <LoginForm />
-    </>
+    <div className='grid grid-cols-2 min-h-[100vh]'>
+      <div className='min-h-full bg-indigo-700 flex items-center min-h-full'>
+        <div className='p-12'>
+          <h1 className='text-white text-4xl font-bold'>Welcome to Admin Portal</h1>
+          <div className='py-8'>
+            <p className='text-white text-sm font-normal'>A product is something a brand is something that is bought by the customer.</p>
+          </div>
+          <Image
+            src='/images/login-footer-brands.svg'
+            alt='Footer Brands'                    
+            width={300}                           
+            height={100}  
+          />
+          
+        </div>
+      </div>
+      <div className='bg-white'>
+        <div className='flex items-center min-h-full w-full justify-center'>
+          <LoginForm />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -5,11 +5,67 @@ const LoginForm: React.FC = () => {
   const { handleLoginSubmit } = useLogin();
 
   return (
-    <>
-      <form onSubmit={handleLoginSubmit}>
-        <button type='submit'>Submit</button>
+    <div>
+      <form className='space-y-6' autoComplete='off' onSubmit={handleLoginSubmit}> 
+        <div className='p-8 w-[450px] mx-auto'>
+          <div>
+            <h2 className='text-3xl text-gray-900  font-semibold'>
+              Sign In
+            </h2>
+            <p className='text-gray-500 text-sm mt-2 mb-10 '>New user? <a className='text-blue-600' href='/register'>Create an Account</a></p>
+          </div>
+          <h6 className='text-base mb-3  text-gray-900  font-medium'>Login with your email id</h6>
+          <div className='mt-2'>
+            <input
+              id='email'
+              name='email'
+              type='email'
+              autoComplete='new-email'
+              className='block px-3 py-4 w-full h-10  rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-indigo-600 autofill:bg-yellow-200 sm:text-sm sm:leading-6'
+            />
+          </div>
+          <div className='mt-4 relative'>
+            <input
+              id='password'
+              name='password'
+              type='password'
+              autoComplete='new-password'
+              className='block px-3 py-4 w-full h-10  rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-indigo-600 autofill:bg-yellow-200 sm:text-sm sm:leading-6'
+            />
+          </div>
+          <div className='flex items-center justify-between mt-2'>
+            <div className='flex items-center gap-2'>
+              <input id='remember' name='remember' type='checkbox' className='checked:bg-blue-500 cursor-pointer w-4 h-4' />
+              <label htmlFor='remember' className='block text-sm font-medium leading-6 text-gray-900 cursor-pointer font-medium text-sm'>
+                Remember me
+              </label>
+            </div>
+            <div className='text-sm'>
+              <a href='#' className=' text-indigo-600 hover:text-indigo-500 font-medium text-[13px] text-rose-600'>
+                Forgot password?
+              </a>
+            </div>
+          </div>
+          <button type='submit'
+            className='flex w-full  mt-6 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+          >
+            Sign in
+          </button>
+        </div>
       </form>
-    </>
+      <div className='reative mt-6'>
+        <div className='flex items-center w-[450px] mx-auto px-9'>
+          <div className='w-full border border-solid border-gray-200'></div>
+        </div>
+        <div className='relative top-[-14px] w-[450px] mx-auto text-center'>
+          <span className='px-2 bg-white'>Or continue with</span>
+        </div>
+      </div>
+      <div className='grid grid-cols-2 gap-2 mt-4  w-[450px] mx-auto px-9'>
+        <a href='#' className='rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 text-center px-3 py-2 hover:bg-gray-50 font-medium'>Google</a>
+        <a href='#' className='rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 text-center px-3 py-2 hover:bg-gray-50 font-medium'>GitHub</a>
+      </div>
+    </div>
   );
 }
 
