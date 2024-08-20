ba45f74a1e119c31dff6ca66c571f0cff95950c3
Usman Ali <aunust786@gmail.com>
2024-08-21 00:10:22 +0500

    branch added NAP-4

---
+++ hooks/useLogout.ts
@@ -0,0 +1,35 @@
+'use client';
+import { useAuthStore } from '@/store/authStore';
+import { useRouter } from 'next/navigation';
+import useApi from './useAPI'
+
+interface LogoutHook {
+  handleLogout: () => Promise<void>;
+}
+
+const useLogout = (): LogoutHook => {
+  const { logout, refreshToken } = useAuthStore();
+  const router = useRouter();
+  const { request } = useApi();
+
+  const handleLogout = async () => {
+    try {
+      await request('/api/auth/logout', 'post', { refreshToken });
+
+      document.cookie = 'accessToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+      document.cookie = 'refreshToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+
+      logout();
+
+      router.push('/login');
+    } catch (error) {
+      console.error('Failed to logout:', error);
+    }
+  };
+
+  return {
+    handleLogout,
+  };
+};
+
+export default useLogout;
