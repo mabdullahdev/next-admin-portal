2961b5970ffe787598c6c653a081ef7da60f109b
samiUllah732 <samiikonic@gmail.com>
2024-08-21 23:35:10 +0500

    confirm button styling

--- components/UI/modal/warningModal.tsx
+++ components/UI/modal/warningModal.tsx
@@ -40,7 +40,7 @@ const WarningModal = (props: ModalProps): ReactNode => {
           type='button'
           data-autofocus
           onClick={onConfirmBtn}
-          className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
+          className='mt-3 inline-flex w-full justify-center rounded-md bg-green-600 px-4 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-green-600 hover:bg-green-500 sm:mt-0 sm:w-auto'
         >
           Confirm
         </button>
